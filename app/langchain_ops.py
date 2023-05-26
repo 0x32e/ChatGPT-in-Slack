5bab1e8ab1da55b606e6624dbc12b5681bf9c007
0x32e <0x32e@protonmail.com>
2023-05-26 19:20:17 -0400

    Update the prompt

--- app/langchain_ops.py
+++ app/langchain_ops.py
@@ -26,7 +26,7 @@ from langchain.memory import ConversationBufferMemory
 from langchain.tools import StructuredTool
 from llama_index import GPTVectorStoreIndex, download_loader, GPTVectorStoreIndex
 
-_llm = ChatOpenAI(temperature=0.0)
+from langchain.utilities import PythonREPL
 
 import os
 
@@ -35,6 +35,9 @@ if os.environ.get("GPLACES_API_KEY") == "":
 
 if os.environ.get("SERPAPI_API_KEY") == "":
     raise Exception("Environment variable SERPAPI_API_KEY is missing")
+
+_llm = ChatOpenAI(temperature=0.0)
+_python_repl = PythonREPL()
 
 # UnstructuredURLLoader = download_loader("UnstructuredURLLoader")
 
@@ -56,7 +59,14 @@ _tools = load_tools(
     llm=_llm
 )
 
-# _tools.append(summarizeWebpage)
+# You can create the tool to pass to an agent
+repl_tool = Tool(
+    name="python_repl",
+    description="A Python shell. Use this to execute python commands. Input should be a valid python command. If you want to see the output of a value, you should print it out with `print(...)`. You can also use this if the other Calculator cannot execute the operation.",
+    func=_python_repl.run
+)
+
+_tools.append(repl_tool)
 
 _memory = ConversationBufferMemory(memory_key="chat_history")
 
@@ -69,16 +79,19 @@ _agent_chain = initialize_agent(
     memory=_memory,
 )
 
-# default_template = """
-# I want you to act as a summarizer who summarizes the messages you received as input. Below is the list of the messages in the ascending order of the time they were sent. 
-# Note that each message string takes the following format: "role: content", and the user your are talking to is the one with the role "user" with the user_id of {user_id}.
-# Output should be a simple one or two sentence summary of the conversations between you and the user.
+default_template = """[SYSTEM]:
+You are a helpful conversational agent who helps the user with their questions. 
+Current date is {current_time}.
 
-# Messages:
-# {messages}
-# """
+[USER]:
+{user_input}
 
-default_template = """{user_input}"""
+[CONTEXT]:
+- You have access to the following tools: {tools}
+- You are talking to the user with the user_id of {user_id}
+- The user is based in New York City
+- When you are asked about the current time, use the current date above.
+"""
 
 # def _convert_to_user_input_and_context(messages: List[Dict[str, str]]):
 #     last_user_input = ""
@@ -111,6 +124,28 @@ def _get_last_user_input(messages: List[str]) -> str:
         raise Exception("No user input found")
     return last_user_input
 
+def _get_tool_names() -> str:
+     tools = [f"Name: {tool.name}, description: {tool.description}" for tool in _tools]
+     return "\n".join(tools)
+
+def _get_current_time() -> str:
+     # set the timezone offset for ET
+    tz_offset = -4 * 60 * 60
+
+    # get the current time in seconds since the Epoch
+    current_time = time.time()
+
+    # add the timezone offset to the current time
+    et_time = current_time + tz_offset
+
+    # convert the Unix timestamp to a datetime object
+    et_datetime = time.strftime('%m/%d/%Y, %I:%M:%S %p', time.localtime(et_time))
+
+    # append the timezone abbreviation
+    et_datetime += ' ET'
+
+    return et_datetime
+
 def execute_agent(
         messages: List[Dict[str, str]], # message format: ["role": str, "content": str]
         user_id: str,
@@ -118,9 +153,14 @@ def execute_agent(
         # Use LangChain's template to add a list of messages to the context
         print("Running agent...")
         prompt = PromptTemplate(
-             input_variables=["user_input"],
+             input_variables=["user_input", "current_time", "tools", "user_id"],
              template=default_template,
         )
         last_user_input = _get_last_user_input(messages)
-        formattedPrompt = prompt.format(user_input=last_user_input)
+        formattedPrompt = prompt.format(
+             user_input=last_user_input, 
+             current_time=_get_current_time(),
+             tools=_get_tool_names(),
+             user_id=user_id,
+        )
         return _agent_chain.run(input=formattedPrompt)
